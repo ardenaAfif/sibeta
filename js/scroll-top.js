02176ba2695c2e86135423b5f4fcd0066ffb60cd
ardenaAfif <aafifpratama@gmail.com>
2023-02-11 23:41:37 +0700

    added pages for content in line 2 context pelajaran

--- js/scroll-top.js
+++ js/scroll-top.js
@@ -1,7 +1,7 @@
 const scrollToTopButton = document.getElementById("scroll-top-button");
 
 window.addEventListener("scroll", () => {
-  if (window.pageYOffset > 100) {
+  if (window.pageYOffset > 20) {
     scrollToTopButton.style.display = "block";
   } else {
     scrollToTopButton.style.display = "none";
